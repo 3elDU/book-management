5c8434337d56eb8c0e5276a8b85df9c245981dfb
Zakhar Voloshchuk (3elDU) <3elduu@gmail.com>
2023-02-24 19:20:19 +0200

    Modified table creation query a bit, added structure with a json schema for a book, added route for creating a new book.

--- main.go
+++ main.go
@@ -21,12 +21,21 @@ func create_table(db *sql.DB) {
 	log.Println(res, err)
 
 	res, err = db.Exec(`CREATE TABLE books (
-		name VARCHAR(128),
-		author VARCHAR(64),
-		pages INT,
+		id SERIAL PRIMARY KEY,
+		name VARCHAR(128) NOT NULL,
+		author VARCHAR(64) NOT NULL,
+		pages INT NOT NULL,
 		filename TEXT
 	);`)
 	log.Println(res, err)
+}
+
+type Book struct {
+	ID       string
+	Name     string `json:"name"`
+	Author   string `json:"author"`
+	Pages    int    `json:"pages"`
+	Filename string
 }
 
 func main() {
@@ -34,8 +43,21 @@ func main() {
 	create_table(db)
 
 	r := gin.Default()
+
 	r.GET("/", func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, "Hello, world!")
 	})
+
+	r.POST("/new_book", func(ctx *gin.Context) {
+		var book Book
+		if err := ctx.BindJSON(&book); err != nil {
+			log.Printf("/new_book ctx.BindJson() error - %v", err)
+			return
+		}
+
+		res, err := db.Exec("INSERT INTO books (name, author, pages) VALUES ($1, $2, $3)", book.Name, book.Author, book.Pages)
+		log.Println(res, err)
+	})
+
 	r.Run()
 }
